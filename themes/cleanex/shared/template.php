b34016029236c4d9a0048c45968e9ce00fd6fd65
avalonsky <gakiyukr@proton.me>
2022-11-23 21:58:48 +0800

    Update

--- themes/cleanex/shared/template.php
+++
@@ -1,8 +0,0 @@
-<?php defined("APP") or die() ?>
-<section<?php echo Main::body_class() ?>>
-  <div class="container">
-  	<div class="is404">
-			<?php echo $content ?>
-  	</div>
-  </div>
-</section>
--- themes/infinity/shared/template.php
+++
@@ -1,8 +0,0 @@
-<?php defined("APP") or die() ?>
-<section class="under-head-cont">
-  <div class="container">
-  	<div class="centered is404">
-			<?php echo $content ?>
-  	</div>
-  </div>
-</section>
